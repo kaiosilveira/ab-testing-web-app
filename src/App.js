1ec79425a794034f84e8de0b6c7a445780355f45
Kaio Silveira <silveira.kaio@icloud.com>
2022-07-23 06:12:45 +0100

    introduce Home screen

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import TaskBlocks from './components/TaskBlocks';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TasksScreen from './screens/Tasks';
+import Home from './screens/Home';
 import * as S from './styles';
-
-const TASK_BLOCK_ITEMS = [
-  {
-    id: 1,
-    title: 'Getting started',
-    description: 'Group of tasks to help you familiarize with the product',
-    taskCount: 4,
-  },
-  {
-    id: 2,
-    title: 'Our suggestions',
-    description: 'Group of tasks that we think will help you move forward',
-    taskCount: 2,
-  },
-  {
-    id: 3,
-    title: 'Becoming a PRO',
-    description: 'Master our most advanced features with these tasks',
-    taskCount: 7,
-  },
-];
 
 const App = () => {
   return (
-    <S.App>
-      <h1>AB TESTING APP</h1>
-      <TaskBlocks taskBlockItems={TASK_BLOCK_ITEMS} />
-    </S.App>
+    <Router>
+      <S.App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<TasksScreen />} />
+        </Routes>
+      </S.App>
+    </Router>
   );
 };
 
---
+++ src/screens/Home/index.js
@@ -0,0 +1,8 @@
+const Home = () => (
+  <>
+    <h1>A/B Testing App</h1>
+    <p>This is the home screen of our app</p>
+  </>
+);
+
+export default Home;
